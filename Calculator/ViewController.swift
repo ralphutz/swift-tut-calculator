6a5a1eb16624c7eba9a0008c287f7ee56d346bf7
Ralph Utz <ralphutz@gmail.com>
2017-02-11 06:34:18 -0600

    main button layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func digit(_ sender: UIButton) {
+        if let digitPressed = sender.currentTitle {
+            
+        }
+        print("Pressed \(digitPressed) digit")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    @IBOutlet weak var display: UILabel!
 
 }
 
